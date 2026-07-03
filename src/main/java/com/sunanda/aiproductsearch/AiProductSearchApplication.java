58c59360f7574ca9fd2a592f026be58377ebd149
skaluva9 <skaluva9@gmail.com>
2026-07-03 19:52:08 -0400

    Create AiProductSearchApplication.java

---
+++ src/main/java/com/sunanda/aiproductsearch/AiProductSearchApplication.java
@@ -0,0 +1,12 @@
+package com.sunanda.aiproductsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProductSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProductSearchApplication.class, args);
+    }
+}
