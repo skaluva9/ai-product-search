65ef872cd0d7941ce6662dde0f82f0b137795fb1
skaluva9 <skaluva9@gmail.com>
2026-07-03 20:03:15 -0400

    Create Product.java

---
+++ src/main/java/com/sunanda/aiproductsearch/model/Product.java
@@ -0,0 +1,51 @@
+package com.sunanda.aiproductsearch.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(length = 2000)
+    private String description;
+
+    private String category;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
